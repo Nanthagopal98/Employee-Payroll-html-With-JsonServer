8dbf74fbbe658acb249a4a5807681a87ce86400b
unknown <s.nanthagopal1998@gmail.com>
2022-07-12 16:09:27 +0530

    [Nantha] - New Payroll Class Created With Get Set

---
+++ js/PayrollClass.js
@@ -0,0 +1,70 @@
+class PayrollClass{
+
+    get  id(){
+        return this._id
+    }
+    set id(id){
+        this._id = id;
+    }
+
+    get empName(){
+        return this._empName;
+    }
+    set empName(empName){
+        let nameRegex = RegExp('^[A-Z]{1}[A-Za-z]{2,}$');
+        if(nameRegex.test(empName)){
+            this._empName = empName;
+    }
+    else{
+        throw "Incorrect Name";
+    }
+    }
+
+    get  empProfile(){
+        return this._empProfile;
+    }
+    set empProfile(empProfile){
+        this._empProfile = empProfile;
+    }
+
+    get  gender(){
+        return this._gender;
+    }
+    set gender(gender){
+        this._gender = gender;
+    }
+
+    get  dept(){
+        return this._dept;
+    }
+    set dept(dept){
+        this._dept = dept;
+    }
+
+    get  salary(){
+        return this._salary;
+    }
+    set salary(salary){
+        this._salary = salary;
+    }
+
+    get  startDate(){
+        return this._startDate;
+    }
+    set startDate(startDate){
+        this._startDate = startDate;
+    }
+
+    get  note(){
+        return this._note;
+    }
+    set note(note){
+        this._note = note;
+    }
+
+    toString(){
+        return "Id: " + this.id + "\nName: " + this.empName + "\nProfile Path: " + this.empProfile +
+            "\nGender: " + this.gender + "\nDepartment: "+ this.dept +"\nSalary: " + this.salary +
+            "\nStart Date: " + this.startDate +"\nNote: "+this.note;
+    }
+    }
