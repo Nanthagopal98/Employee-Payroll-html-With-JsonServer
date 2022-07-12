43a2dbc442128a85cb9e7ab0294f1072aa991fb1
unknown <s.nanthagopal1998@gmail.com>
2022-07-12 16:14:01 +0530

    [Nantha] - Onclick Event Added For Employee Name

--- js/EmployeePayrollScript.js
+++ js/EmployeePayrollScript.js
@@ -4,7 +4,23 @@ salary.addEventListener('input', function(){
     outputSalary.textContent = salary.value; 
 });
 
-
+window.addEventListener('DOMContentLoaded',(event) =>{
+    const  NAME = document.querySelector('#name');
+    const ERROR_OP = document.querySelector('.error-output');
+    NAME.addEventListener('input',function(){
+        if(NAME.value.length == 0){
+            ERROR_OP.textContent = "";
+            return;
+        }
+        try{
+            (new PayrollClass()).empName = NAME.value;
+            ERROR_OP.textContent = "";
+        }
+        catch(e){
+            ERROR_OP.textContent = e;
+        }
+    });
+});
 
 
 
