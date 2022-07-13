0af6c4116d8819d9ec4a51d2b25c76738eed2fc5
unknown <s.nanthagopal1998@gmail.com>
2022-07-13 17:11:25 +0530

    [Nantha] - Display Employee Detail From JSON Object

--- js/home.js
+++ js/home.js
@@ -5,17 +5,17 @@ window.addEventListener("DOMContentLoaded", (event) =>{
 const createInnetHtml = () =>{
     const tableheader = "<tr><th>Profile</th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th>"+
         "<th>Start Date</th><th>Action</th></tr>";
-
+    let employeePayrollData = createEmployeePayrollJSON()[1];
     const innerHtlm = `${tableheader}
      <tr>
-        <td><img class="profile" alt="Profile Photo" src="../assets/pro2.jpg"></td>
-        <td>Nantha</td>
-        <td>Male</td>
-        <td><div class="dept">Engineer</div>
-            <div class="dept">HR</div>
+        <td><img class="profile" alt="Profile Photo" src="${employeePayrollData._empProfile}"></td>
+        <td>${employeePayrollData._empName}</td>
+        <td>${employeePayrollData._gender}</td>
+        <td><div class="dept">${employeePayrollData._dept[0]}</div>
+            <div class="dept">${employeePayrollData._dept[1]}</div>
         </td>
-            <td>30000</td>
-            <td>01 April 2022</td>
+            <td>${employeePayrollData._salary}</td>
+            <td>${employeePayrollData._startDate}</td>
         <td>
             <img id="delete" onclick="remove(this)" class="action_img" src="../assets/trashbin.jpeg">
             <img id="update" onclick="update(this)" class="action_img" src="../assets/add.jpeg">
@@ -23,4 +23,30 @@ const createInnetHtml = () =>{
     </tr>
 `;
 document.querySelector('#table-display').innerHTML = innerHtlm;
+}
+
+const createEmployeePayrollJSON = () => {
+    let employeePayrollList = [
+        {
+            _empProfile : '../assets/pro1.jpg',
+            _empName : 'Nantha',
+            _gender : 'Male',
+            _dept : ['Engineering','HR'],
+            _salary : '30000',
+            _startDate : '01 Jan 2022',
+            _note : '',
+            _id : new Date().getTime(),
+        },
+        {
+            _empProfile : '../assets/pro1.jpg',
+            _empName : 'Ela',
+            _gender : 'Male',
+            _dept : ['Engineering'],
+            _salary : '35000',
+            _startDate : '01 Jan 2022',
+            _note : '',
+            _id : new Date().getTime(),
+        }
+    ];
+    return employeePayrollList;
 }
