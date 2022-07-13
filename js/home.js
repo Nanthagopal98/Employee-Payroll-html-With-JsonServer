c43407c76de10ca77db3ed8c793a84ac5776d1ed
unknown <s.nanthagopal1998@gmail.com>
2022-07-13 16:04:04 +0530

    [Nantha] - Template Literal added To Table

---
+++ js/home.js
@@ -0,0 +1,32 @@
+window.addEventListener("DOMContentLoaded", (event) =>{
+    createInnetHtml();
+});
+
+const createInnetHtml = () =>{
+    const innerHtlm = `
+    <tr>
+        <th>Profile</th>
+        <th>Name</th>
+        <th>Gender</th>
+        <th>Department</th>
+        <th>Salary</th>
+        <th>Start Date</th>
+        <th>Action</th>
+    </tr>
+     <tr>
+        <td><img class="profile" alt="Profile Photo" src="../assets/pro2.jpg"></td>
+        <td>Nantha</td>
+        <td>Male</td>
+        <td><div class="dept">Engineer</div>
+            <div class="dept">HR</div>
+        </td>
+            <td>30000</td>
+            <td>01 April 2022</td>
+        <td>
+            <img id="delete" onclick="remove(this)" class="action_img" src="../assets/trashbin.jpeg">
+            <img id="update" onclick="update(this)" class="action_img" src="../assets/add.jpeg">
+        </td>
+    </tr>
+`;
+document.querySelector('#table-display').innerHTML = innerHtlm;
+}
