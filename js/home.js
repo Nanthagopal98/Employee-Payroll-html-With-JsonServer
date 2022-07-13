0951219a0d5e59ac75f5c463b94b37e5ea01a805
unknown <s.nanthagopal1998@gmail.com>
2022-07-13 18:04:27 +0530

    [Nantha] - Display Employee Detail From JSON Object Using For Loop

--- js/home.js
+++ js/home.js
@@ -2,33 +2,34 @@ window.addEventListener("DOMContentLoaded", (event) =>{
     createInnetHtml();
 });
 
-const createInnetHtml = () =>{
-    const tableheader = "<tr><th>Profile</th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th>"+
+const createInnetHtml = () => {
+    const tableheader = "<tr><th>Profile</th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th>" +
         "<th>Start Date</th><th>Action</th></tr>";
-    let employeePayrollData = createEmployeePayrollJSON()[1];
-    const innerHtlm = `${tableheader}
+    let innerHtlm = `${tableheader}`;
+    let getData = createEmployeePayrollJSON();
+    for (const employeePayrollData of getData) {
+        innerHtlm = `${innerHtlm}
      <tr>
         <td><img class="profile" alt="Profile Photo" src="${employeePayrollData._empProfile}"></td>
         <td>${employeePayrollData._empName}</td>
         <td>${employeePayrollData._gender}</td>
-        <td><div class="dept">${employeePayrollData._dept[0]}</div>
-            <div class="dept">${employeePayrollData._dept[1]}</div>
-        </td>
-            <td>${employeePayrollData._salary}</td>
-            <td>${employeePayrollData._startDate}</td>
+        <td>${getDeptHtml(employeePayrollData._dept)}</td>
+        <td>${employeePayrollData._salary}</td>
+        <td>${employeePayrollData._startDate}</td>
         <td>
             <img id="delete" onclick="remove(this)" class="action_img" src="../assets/trashbin.jpeg">
             <img id="update" onclick="update(this)" class="action_img" src="../assets/add.jpeg">
         </td>
     </tr>
 `;
-document.querySelector('#table-display').innerHTML = innerHtlm;
+    }
+    document.querySelector('#table-display').innerHTML = innerHtlm;
 }
 
 const createEmployeePayrollJSON = () => {
     let employeePayrollList = [
         {
-            _empProfile : '../assets/pro1.jpg',
+            _empProfile : '../assets/pro2.jpg',
             _empName : 'Nantha',
             _gender : 'Male',
             _dept : ['Engineering','HR'],
@@ -38,7 +39,7 @@ const createEmployeePayrollJSON = () => {
             _id : new Date().getTime(),
         },
         {
-            _empProfile : '../assets/pro1.jpg',
+            _empProfile : '../assets/pro4.jpg',
             _empName : 'Ela',
             _gender : 'Male',
             _dept : ['Engineering'],
@@ -49,4 +50,12 @@ const createEmployeePayrollJSON = () => {
         }
     ];
     return employeePayrollList;
+}
+
+const getDeptHtml = (deptList) =>{
+    let deptHtml = '';
+    for (const dept of deptList){
+        deptHtml = `${deptHtml} <div class="dept">${dept}</div>`
+    }
+    return deptHtml
 }
